1e10f654953bda5fc9ab25bb156eae173e9e91bf
Javier Quinteros <javier@nora.cl>
2021-10-06 18:39:50 -0400

    Se implementó otro lint y se corrigió el código.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'Router/ui/pages/route_page.dart';
+import 'package:adaus/Router/ui/pages/router_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+// ignore: public_member_api_docs
 class MyApp extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
