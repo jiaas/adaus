539fa629fa97768e2901152fc2ba92c11970303c
Javier Quinteros <javier@nora.cl>
2021-10-06 17:49:27 -0400

    Implementación de Firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'Router/ui/pages/router_page.dart';
+import 'Router/ui/pages/route_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
